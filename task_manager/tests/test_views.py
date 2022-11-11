1716a88505f6fba4f7c255e26a54223dad8ce056
mltdll <ilya.vyplavin@gmail.com>
2022-11-11 05:48:43 +0200

    Add tests for `TaskType` views

--- task_manager/tests/test_views.py
+++ task_manager/tests/test_views.py
@@ -1,7 +1,9 @@
 import pytest
 from django.urls import reverse
 from django.utils.http import urlencode
-from pytest_django.asserts import assertRedirects
+from pytest_django.asserts import assertRedirects, assertTemplateUsed
+
+from ..models import TaskType
 
 
 @pytest.mark.parametrize(
@@ -120,3 +122,65 @@ def test_login_required(url, client):
         f"{reverse('login')}?{urlencode({'next': url})}",
         status_code=302,
     )
+
+
+class TestTaskTypeViews:
+    @pytest.mark.django_db
+    def test_task_type_list(self, task_type_data, employee_client):
+        response = employee_client.get(reverse("task_manager:task-type-list"))
+        task_types = TaskType.objects.all()
+
+        assert response.status_code == 200
+        assert list(response.context["task_type_list"]) == list(task_types)
+
+    @pytest.mark.django_db
+    def test_task_type_template(self, employee_client):
+        response = employee_client.get(reverse("task_manager:task-type-list"))
+
+        assertTemplateUsed(response, "task_manager/task_type_list.html")
+
+    @pytest.mark.django_db
+    def test_create_task_type(self, employee_client):
+        form_data = {"name": "Test1"}
+
+        response = employee_client.post(
+            reverse("task_manager:task-type-create"), data=form_data
+        )
+
+        assert response.status_code == 302
+        assert TaskType.objects.filter(name=form_data["name"]).exists()
+
+    @pytest.mark.django_db
+    def test_update_task_type(self, employee_client, task_type_data):
+        form_data = {"name": "Extra task"}
+        entry_id = 1
+
+        response = employee_client.post(
+            reverse("task_manager:task-type-update", args=[entry_id]),
+            data=form_data,
+        )
+
+        task_type = TaskType.objects.get(pk=entry_id)
+
+        assert response.status_code == 302
+        assert task_type.name == form_data["name"]
+
+    @pytest.mark.django_db
+    def test_delete_task_type(self, employee_client, task_type_data):
+        entry_id = 1
+
+        response = employee_client.post(
+            reverse("task_manager:task-type-delete", args=[entry_id])
+        )
+
+        assert response.status_code == 302
+        assert not TaskType.objects.filter(pk=entry_id).exists()
+
+    @pytest.mark.django_db
+    def test_search_task_type(self, employee_client, task_type_data):
+        response = employee_client.get(
+            reverse("task_manager:task-type-list") + "?name=y"
+        )
+        task_types = TaskType.objects.filter(name__icontains="y")
+
+        assert list(response.context["task_type_list"]) == list(task_types)
